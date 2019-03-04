e04d3ba5332d02e52eccf430ad08d41ddbc3c5e8
Samuel <spilkey1234@gmail.com>
2019-03-04 12:16:50 -0500

    inital comments

--- Output.java
+++ Output.java
@@ -4,30 +4,48 @@ public class Output{
 
     }
 
+    /*
+    * Function can take in an arraylist with all the inputs
+    *
+    */
     public void createUser(){
 
     }
 
+    /*
+    *
+    *
+    */
     public void sellTickets(){
 
     }
 
+    /*
+    *
+    *
+    */
     public void addCredit(){
 
     }
 
+    /*
+    *
+    *
+    */
     public void deleteUser(){
 
-    } 
+    }
 
+    /*
+    *
+    *
+    */
     public void refundUser(){
 
     }
 
     public void buyTickets(){
-        
+
     }
-    
+
 }
-
-
