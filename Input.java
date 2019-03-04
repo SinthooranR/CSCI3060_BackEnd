a798bbccbbe35e6751174d132b380a49e39dd3d9
Sinthooran Ravinathan <sinthooran.ravinathan@uoit.net>
2019-03-04 11:52:07 -0500

    Added base source files and the Makefile to compile all the java files

---
+++ Input.java
@@ -0,0 +1,11 @@
+public class Input{
+
+
+    public Input(){
+    
+    
+    }
+
+ 
+
+}
---
+++ Output.java
@@ -0,0 +1,33 @@
+public class Output{
+
+    public Output(){
+
+    }
+
+    public void createUser(){
+
+    }
+
+    public void sellTickets(){
+
+    }
+
+    public void addCredit(){
+
+    }
+
+    public void deleteUser(){
+
+    } 
+
+    public void refundUser(){
+
+    }
+
+    public void buyTickets(){
+        
+    }
+    
+}
+
+
