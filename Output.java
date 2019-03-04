f18101863c72e6757927462cff728079723ba4eb
Sinthooran Ravinathan <sinthooran.ravinathan@uoit.net>
2019-03-04 15:49:01 -0500

    Tested some substring and implemented the initial ArrayLists

--- Output.java
+++ Output.java
@@ -1,7 +1,13 @@
 import java.io.*;
+import java.util.*;
 
 public class Output{
 
+String line = "";
+List<String> daily_trans = new ArrayList<String>();
+List<String> user_file = new ArrayList<String>();
+List<String> tickets_file = new ArrayList<String>();
+    
     public Output(){
 
     }
@@ -15,13 +21,21 @@ public class Output{
     public void fileReader(String filename){
     try{
         File file = new File(filename);
+        int count = 0;
         FileReader fReader = new FileReader(file);
         BufferedReader bReader = new BufferedReader(fReader);
 
-        String line = "";
         while ((line = bReader.readLine()) != null){
-            System.out.println(line);
+            count++; //Checks if the File is reading line by line
+            //Still Needs Work
+            String usernames = line.substring(3,18); //Has the substrings for Users
+            String eventnames = line.substring(3,28); //Has the suibstrings for Event names
+            String ticketPrices = line. 
+            System.out.println(lines);
+
         }
+        System.out.println("Total Lines in File:" + " " + count); //Shows how many lines exist in the file
+
         bReader.close();
     }
     catch(Exception e){
